be08206167764f80ea8592e8662aa6c78266a18e
Zachary Flight <zachary@grotonbiblechapel.org>
2025-06-21 11:45:24 -0400

    Updated index

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
-  include('pretitle.php');
+  include('pretitle.php'); $urlvars="";
 
   if(isset($_GET['plot']))
   {
     $getid=$_GET['plot'];
-    $plot="SELECT Plot_ID, Plot_Name FROM StagePlots WHERE (Plot_ID='$getid')"; $plotid=""; $name="";
+    $plot="SELECT Plot_ID, Plot_Name, Plot_Location FROM StagePlots WHERE (Plot_ID='$getid')"; $plotid=""; $location=""; $name="";
     if(!$rs=mysqli_query($db,$plot)) { echo("Unable to Run Query: $plot"); exit; }
-    while($row = mysqli_fetch_array($rs)) { $plotid=$row['Plot_ID']; $name=$row['Plot_Name']; }
+    while($row = mysqli_fetch_array($rs)) { $plotid=$row['Plot_ID']; $name=$row['Plot_Name']; $location=$row['Plot_Location']; }
+    $urlvars="<input type='hidden' name='location' value='$location' />";
   }
   elseif(isset($_GET['location']))
   {
@@ -19,13 +20,14 @@
       $start1=strtotime($row['Plot_Start1']);
       $start2=strtotime($row['Plot_Start2']);
       $start3=strtotime($row['Plot_Start3']);
-  
+
       if($start1 <= time()) { $idarray[$start1]=$id; }
       if($start2 <= time()) { $idarray[$start2]=$id; }
       if($start3 <= time()) { $idarray[$start3]=$id; }
     }
     ksort($idarray);
     if(count($idarray) > 0) { $plotid=end($idarray); $name=$names[$plotid]; }
+    $urlvars="<input type='hidden' name='location' value='$location' />";
   }
   else
   {
@@ -39,7 +41,7 @@
   }
 
   echo("<title>$name Stage Plot</title>");
-  
+
   include('posttitle.php');
 
   if ($plotid != "")
@@ -47,8 +49,7 @@
     echo("<div class='viewplotname'><br>$name</div><br>");
     if(file_exists("files/$plotid.png")) { echo("<img src='files/$plotid.png' class='viewplotimage'>"); }
     else { echo("<div class='viewplotname'><br><br>No Plot File Available</div><br>"); }
+    echo("<form method='get' action='index.php' style='text-align:center'>$urlvars<input type='submit' value='REFRESH' /></form>");
   }
-
-  echo("<form action='index.php' style='text-align:center'><input type='submit' value='REFRESH' /></form>");
   include('footer.php');
 ?>
